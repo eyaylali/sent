121f6f9e13a48ede80289e96d3b54193d5dd09ca
Erin Yaylali <erinyaylali@gmail.com>
2015-02-16 15:48:52 -0800

    fixing comments in load_zendesk.py

--- load_zendesk.py
+++ load_zendesk.py
@@ -21,24 +21,24 @@ new_ticket = {
 
 result = zendesk.ticket_create(data=new_ticket)
 
-# def main(file):
-# 	precontent = open(jsonfile).read()
-# 	content = json.loads(precontent)
+def main(file):
+	precontent = open(jsonfile).read()
+	content = json.loads(precontent)
 
-# 	for review in content["Reviews"]:
-# 		new_ticket["ticket"]["requester_name"] = review["Author"].encode('ascii','ignore')
-# 		new_ticket["ticket"]["requester_email"] = "customer@email.com"
-# 		new_ticket["ticket"]["subject"] = review["Title"].encode('ascii','ignore')
-# 		new_ticket["ticket"]["comment"]["body"] = review["Content"].encode('ascii','ignore')
+	for review in content["Reviews"]:
+		new_ticket["ticket"]["requester_name"] = review["Author"].encode('ascii','ignore')
+		new_ticket["ticket"]["requester_email"] = "customer@email.com"
+		new_ticket["ticket"]["subject"] = review["Title"].encode('ascii','ignore')
+		new_ticket["ticket"]["comment"]["body"] = review["Content"].encode('ascii','ignore')
 
-# 		# Create the ticket and get its URL
-# 		result = zendesk.ticket_create(data=new_ticket)
+		# Create the ticket and get its URL
+		result = zendesk.ticket_create(data=new_ticket)
 
-# 	jsonfile.close()
+	jsonfile.close()
 
-# if __name__ == "__main__":
-#     input_file_1 = argv[1]
-#     main(input_file_1)
+if __name__ == "__main__":
+    input_file_1 = argv[1]
+    main(input_file_1)
 
 # # Show
 # zendesk.ticket_show(id=ticket_id)
