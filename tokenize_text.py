1bfaba4c7e20465a6e9147d38cf9d6cb1a0f13e3
Erin Yaylali <erinyaylali@gmail.com>
2015-02-18 23:51:50 -0800

    successfully preserving case for words in all caps

--- tokenize_text.py
+++ tokenize_text.py
@@ -6,10 +6,9 @@ from sys import argv
 
 COMMON_WORDS = ["a","able","about","across","after","all","almost","also","am","among","an","and","any","are","as","at","be","because","been","but","by","can","cannot","could","dear","did","do","does","either","else","ever","every","for","from","get","got","had","has","have","he","her","hers","him","his","how","however","i","if","in","into","is","it","its","just","least","let","like","likely","may","me","might","most","must","my","neither","no","nor","not","of","off","often","on","only","or","other","our","own","rather","said","say","says","she","should","since","so","some","than","that","the","their","them","then","there","these","they","this","tis","to","too","twas","us","wants","was","we","were","what","when","where","which","while","who","whom","why","will","with","would","yet","you","your"]
 NEGATION = ["never","no","nothing","nowhere","noone","none","not","havent","hasnt","hadnt","cant","couldnt","shouldnt","wont","wouldnt","dont","doesnt","didnt","isnt","arent","aint"]
-CLAUSE_PUNC =["^","[",".",":",";","!","?","]","$",","]
 
 stopwords = nltk.corpus.stopwords.words('english')
-print stopwords
+
 sentence_re = r'''(?x)
 		#abbreviations
 		([A-Z])(\.[A-Z])+\.?
@@ -24,7 +23,7 @@ sentence_re = r'''(?x)
 		'''
 all_tokenized_reviews_dict = {}
 
-def unpack(input_file):
+def unpack_and_tokenize(input_file):
 
 	opened_file = open(input_file).read()
 	content = json.loads(opened_file)
@@ -40,39 +39,35 @@ def unpack(input_file):
 		tokens_title = nltk.regexp_tokenize(title, sentence_re)
 		tokens_content = nltk.regexp_tokenize(review_content, sentence_re)
 
-		print tokens_all
-
 		tokens_all_final = [w for w in tokens_all if w.lower() not in stopwords]
 		tokens_title_final = [w for w in tokens_title if w.lower() not in stopwords]
 		tokens_content_final = [w for w in tokens_content if w.lower() not in stopwords]
-		print tokens_all_final
-		tokenized_dict = {"Rating":review["Ratings"]["Overall"], "Title": tokens_title_final, "Author":author, "Content":tokens_content_final, "JoinedContent":tokens_all_final}
+
+		content = [w.lower() if w.isupper() != True else w for w in tokens_all_final]
+		
+		tokenized_dict = {"Rating":review["Ratings"]["Overall"], "Title": tokens_title_final, "Author":author, "Content":tokens_content_final, "JoinedContent":content}
 		all_tokenized_reviews_dict[id] = tokenized_dict
 
-	# return all_tokenized_reviews_dict
-	# print opened_file
+	return all_tokenized_reviews_dict
 
-# def count(input_list):
-# 	words_count = {}
-# 	for word in input_list:
-# 		if word in IGNORE:
-# 			continue
-# 		else:
 
-# working_dict[word] = working_dict.get(word, 0) + 1
-
+# def preserve_case(main_dict):
+# 	for review in main_dict:
+# 		content = [w.lower() if w.isupper() != True else w for w in review["JoinedContent"]]
+# 		main_dict[review]["JoinedContent"] = content
+# 	return main_dict
 
 
 def main(input_file):
-	
-	unpacked = unpack(input_file)
-	# count1 = count(unpacked[0])
-	print unpacked
-
-
+	tokenized = unpack_and_tokenize(input_file)
+	case = preserve_case(tokenized)
+	print tokenized
 
 if __name__ == "__main__":
     script, input_file = argv
     main(input_file)
 
-
+####TODO####
+#remove ' from negation
+#preserve emoticons
+#think about tweets
