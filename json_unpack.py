104b8441022a87d891dd1bbde07839ee52c2f004
Erin Yaylali <erinyaylali@gmail.com>
2015-02-23 16:49:02 -0800

    looping through files and json objects now controls for duplicates

--- json_unpack.py
+++ json_unpack.py
@@ -9,33 +9,35 @@ import json                          # library for reading json in python
 
 all_tokenized_reviews_dict = {}
 
-def unpack(json_obj):
-	for review in json_obj["Reviews"]:
-		if "Title" not in review or "Content" not in review or "Ratings" not in review:
-			continue
-		title = tokenize_text(review["Title"].encode('ascii','ignore'))
-		review_content = tokenize_text(review["Content"].encode('ascii','ignore'))
-		author = review["Author"].encode('ascii','ignore')
-		id = review["ReviewID"].encode('ascii','ignore')
-		rating = review["Ratings"]["Overall"]
-		all_content_tokenized = title + review_content
+def unpack_review(review):
+	if "Title" not in review or "Content" not in review or "Ratings" not in review or "ReviewID" not in review:
+		return None, None
+	title = tokenize_text(review["Title"].encode('ascii','ignore'))
+	review_content = tokenize_text(review["Content"].encode('ascii','ignore'))
+	author = review["Author"].encode('ascii','ignore')
+	id = review["ReviewID"].encode('ascii','ignore')
+	rating = review["Ratings"]["Overall"]
+	all_content_tokenized = title + review_content
 
-		tokenized_dict = {"Rating": rating, "Content":all_content_tokenized}
-		# if tokenized_dict in all_tokenized_reviews_dict.values():
-		# 	continue
-		all_tokenized_reviews_dict[tokenized_dict] = id
-	return all_tokenized_reviews_dict
+	tokenized_dict = {"Rating": rating, "Content":all_content_tokenized}
+	return id, tokenized_dict
 
 def main():
 	count = 0
 	for filename in glob('data/*.json'):
-		print filename
 		with open(filename, 'r') as f:
 			json_obj = json.load(f)
-			tokenized = unpack(json_obj)
+			for review in json_obj["Reviews"]:
+				review_id, tokenized = unpack_review(review)
+				if review_id and tokenized:
+					print review_id
+					if tokenized not in all_tokenized_reviews_dict.values():
+						all_tokenized_reviews_dict[review_id] = tokenized
+						print tokenized
 			#assign 90% to training set and 10% to testing set
-			count = count +1
-			print tokenized, count
+			# count = count +1
+			
+	
 
 if __name__ == "__main__":
     # script, input_path= argv
