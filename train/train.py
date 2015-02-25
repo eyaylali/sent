98b96721dbc2189d2eec837cdf771ca9f82041d9
Erin Yaylali <erinyaylali@gmail.com>
2015-02-25 13:41:50 -0800

    improved accuracy, adding lables to the start ratings

--- train/train.py
+++ train/train.py
@@ -11,22 +11,22 @@ from sys import argv
 from tokenizer import tokenize_text
 import json
 
-text_clf = Pipeline([('vect', CountVectorizer()),('tfidf', TfidfTransformer()),('clf', MultinomialNB())])
+text_clf = Pipeline([('vect', CountVectorizer()),('tfidf', TfidfTransformer()),('clf', BernoulliNB())])
 
-data = []
-target = []
+text_data = []
+categories = []
 
-def load_files():
+def parse_files():
 	for filename in glob('test-data/*.json'):
 		with open(filename, 'r') as f:
 			json_obj = json.load(f)
 			for review in json_obj["Reviews"]:
-				tokenized, rating = unpack_review(review)
-				if rating and tokenized:
-					if tokenized not in data:
-						target.append(rating)
-						data.append(tokenized)
-	return data, target
+				tokenized, label = unpack_review(review)
+				if label and tokenized:
+					if tokenized not in text_data:
+						categories.append(label)
+						text_data.append(tokenized)
+	return text_data, categories
 
 def unpack_review(review):
 	if "Title" not in review or "Content" not in review or "Ratings" not in review or "ReviewID" not in review:
@@ -38,15 +38,19 @@ def unpack_review(review):
 	rating = review["Ratings"]["Overall"]
 	all_content= title +" "+ review_content
 
-	return all_content, rating
+	if rating == "1.0" or rating == "2.0":
+		label = "upset"
+	elif rating == "3.0":
+		label = "neutral"
+	else:
+		label = "positive"
+	return all_content, label
 
-def preprocess():
-    data,target = load_files()
+def preprocess(data_list, catgory_list):
     vectorizer = CountVectorizer(tokenizer = tokenize_text, lowercase = False)
-    data = vectorizer.fit_transform(data)
-    tfidf_data = TfidfTransformer(use_idf=False).fit_transform(data)
-
-    return tfidf_data
+    data_output = vectorizer.fit_transform(data_list)
+    # tfidf_data = TfidfTransformer(use_idf=False).fit_transform(data_output)
+    return data_output
 
 def learn_model(data,target):
     data_train,data_test,target_train,target_test = cross_validation.train_test_split(data,target,test_size=0.1,random_state=0)
@@ -54,16 +58,14 @@ def learn_model(data,target):
     predicted = classifier.predict(data_test)
     evaluate_model(target_test,predicted)
 
-# read more about model evaluation metrics here
-# http://scikit-learn.org/stable/modules/model_evaluation.html
 def evaluate_model(target_true,target_predicted):
     print classification_report(target_true,target_predicted)
     print "The accuracy score is {:.2%}".format(accuracy_score(target_true,target_predicted))
 
 def main():
-	data,target = load_files()
-	tf_idf = preprocess()
-	learn_model(tf_idf,target)
+	review_data, categories = parse_files()
+	term_freq_matrix = preprocess(review_data, categories)
+	learn_model(term_freq_matrix, categories)
 
 
 if __name__ == "__main__":
